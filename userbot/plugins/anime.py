1cdfe64782a3643487adcb53b8483d2d1be3ddc4
HUNTERXUSERBOT <102667059+HUNTERXUSERBOT@users.noreply.github.com>
2022-03-31 15:22:30 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -10,24 +10,24 @@ from . import *
 @bot.on(admin_cmd(pattern="anime(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="anime(?: |$)(.*)", allow_sudo=True))
 async def nope(h1m4n5hu0p):
-    mafia = h1m4n5hu0p.pattern_match.group(1)
-    if not mafia:
-        if h1m4n5hu0p.is_reply:
-            (await h1m4n5hu0p.get_reply_message()).message
+    hunter = hunterxuserbot.pattern_match.group(1)
+    if not hunter:
+        if hunterxuserbot.is_reply:
+            (await hunterxuserbot.get_reply_message()).message
         else:
-            await edit_or_reply(h1m4n5hu0p, "`Sir please give some query to search and download it for you..!`"
+            await edit_or_reply(hunterx, "`Sir please give some query to search and download it for you..!`"
             )
             return
 
     troll = await bot.inline_query("animedb_bot", f"{(deEmojify(mafia))}")
 
     await troll[0].click(
-        h1m4n5hu0p.chat_id,
-        reply_to=h1m4n5hu0p.reply_to_msg_id,
-        silent=True if h1m4n5hu0p.is_reply else False,
+        hunter.chat_id,
+        reply_to=hunterxuserbot.reply_to_msg_id,
+        silent=True if  hunterxuserbot.is_reply else False,
         hide_via=True,
     )
-    await h1m4n5hu0p.delete()
+    await hunterxuserbot.delete()
     
 
 CmdHelp("anime").add_command(
