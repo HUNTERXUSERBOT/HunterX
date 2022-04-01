69ebca2a2ae1a2c931e768dca17a3b38d7a6c8e5
HUNTERXUSERBOT <102667059+HUNTERXUSERBOT@users.noreply.github.com>
2022-04-01 11:33:58 +0530

    Update filemanager.py

--- userbot/plugins/filemanager.py
+++ userbot/plugins/filemanager.py
@@ -10,7 +10,7 @@ import time
 
 from telethon import events
 
-from mafiabot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from hunterx.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 if not os.path.isdir("./SAVED"):
